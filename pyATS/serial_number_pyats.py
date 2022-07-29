627f18260741e00d308dc234237badb5b8e421b8
Antoine Orsoni <ant.orsoni@gmail.com>
2022-07-29 16:46:27 +0200

    logs

--- pyATS/serial_number_pyats.py
+++ pyATS/serial_number_pyats.py
@@ -4,7 +4,10 @@ testbed = load('./testbed.yaml')
 iosxr = testbed.devices["R1"]
 
 # Connect to the device
-iosxr.connect(init_exec_commands=[], init_config_commands=[], log_stdout=False)
+iosxr.connect(init_exec_commands=[], 
+            init_config_commands=[], 
+            log_stdout=False,
+            learn_hostname=True)
 
 # Collecting the structured output
 show_inventory = iosxr.parse('show inventory')
